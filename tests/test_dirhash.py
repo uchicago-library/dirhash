1aa3a4d9a412f6ee18aa887e4114a43e0bcccaa0
brian <balsamo@uchicago.edu>
2018-01-18 15:17:56 -0600

    Test recursive cases

--- tests/test_dirhash.py
+++ tests/test_dirhash.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 from tempfile import TemporaryDirectory
 from os.path import join
 from os import scandir
+from shutil import copytree
 
 
 class Tests(unittest.TestCase):
@@ -55,6 +56,41 @@ class Tests(unittest.TestCase):
             # Check that they're not equal
             h1 = dirhash.hash_dir(dir1.name)[0].hexdigest()
             h2 = dirhash.hash_dir(dir2.name)[0].hexdigest()
+            self.assertNotEqual(h1, h2)
+
+    def testRecursionSame(self):
+        with TemporaryDirectory() as containing_dir:
+            dir1 = TemporaryDirectory(dir=containing_dir)
+            dir11 = TemporaryDirectory(dir=dir1.name)
+            for x in range(10):
+                with open(join(dir1.name, uuid4().hex), 'w') as f:
+                    f.write(uuid4().hex)
+            for x in range(10):
+                with open(join(dir11.name, uuid4().hex), 'w') as f:
+                    f.write(uuid4().hex)
+            dir2name = join(containing_dir, uuid4().hex)
+            copytree(dir1.name, dir2name)
+            h1 = dirhash.hash_dir(dir1.name)[0].hexdigest()
+            h2 = dirhash.hash_dir(dir2name)[0].hexdigest()
+            self.assertEqual(h1, h2)
+
+    def testRecursionDifferent(self):
+        with TemporaryDirectory() as containing_dir:
+            dir1 = TemporaryDirectory(dir=containing_dir)
+            dir11 = TemporaryDirectory(dir=dir1.name)
+            for x in range(10):
+                with open(join(dir1.name, uuid4().hex), 'w') as f:
+                    f.write(uuid4().hex)
+            for x in range(10):
+                with open(join(dir11.name, uuid4().hex), 'w') as f:
+                    f.write(uuid4().hex)
+            dir2name = join(containing_dir, uuid4().hex)
+            copytree(dir1.name, dir2name)
+            # Add another file to the subdir in dir2
+            with open(join(dir2name, dir11.name, uuid4().hex), 'w') as f:
+                f.write(uuid4().hex)
+            h1 = dirhash.hash_dir(dir1.name)[0].hexdigest()
+            h2 = dirhash.hash_dir(dir2name)[0].hexdigest()
             self.assertNotEqual(h1, h2)
 
 
